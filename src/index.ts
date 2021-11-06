6123d01e85f82da80a8cb37497f21122ab5db257
ckie <git-525ff67@ckie.dev>
2021-11-06 11:34:39 +0200

    write a cute little repl

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import { init } from "./discord";
 import dotenv from "dotenv-safe";
+import { config } from "./config";
+import readline from "readline";
+import { inspect } from "util";
 
 async function main() {
     dotenv.config();
     const client = await init();
+
+    if (!config.production) {
+        const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+        function prompt(): Promise<string> {
+            return new Promise(resolve => {
+                rl.question("> ", answer => resolve(answer));
+            })
+        }
+        while (true) {
+            const js = await prompt();
+            console.log(inspect(eval(js), false, 2, true));
+        }
+    }
+
 }
 
 main().catch(err => { throw err; })
