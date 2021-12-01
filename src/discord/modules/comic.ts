171570b51539b68f0716dd5f9ff6c922ceace0d4
ckie <git-525ff67@ckie.dev>
2021-12-02 01:47:17 +0200

    discord/daiko: so many mistakes oh my god

--- src/discord/modules/comic.ts
+++ src/discord/modules/comic.ts
@@ -2,19 +2,16 @@ import {
     MessageEmbed,
     TextChannel,
     Message,
-    CollectorFilter,
 } from "discord.js";
 import {
     listener,
     default as CookiecordClient,
     Module,
-    command,
-    CommonInhibitors,
 } from "cookiecord";
 import { logger } from "../../logger";
 import { store } from "../../store";
 import { config } from "../../config";
-import { ComicSource, fetchers, isComicSource } from "../../api/comic";
+import { ComicSource, fetchers } from "../../api/comic";
 import { URL } from "url";
 
 export default class ComicPoller extends Module {
@@ -38,22 +35,20 @@ export default class ComicPoller extends Module {
             const meta = await fetchers[comic.source](new URL(comic.url))();
             const latest = meta.pages[meta.pages.length - 1];
 
-            if (comic.maxPageId < latest.id) {
+            if (comicState.maxPageId < latest.id) {
                 const embed = new MessageEmbed();
                 embed.setAuthor(meta.title, undefined, latest.url);
                 embed.setImage(meta.bannerURL);
                 embed.setURL(latest.url);
                 embed.setTitle(latest.title);
 
-                const chan = await this.client.channels.fetch(
-                    process.env.COMICS_CHANNEL_ID!
-                );
+                const chan = await this.client.channels.fetch(this.config.channelId);
                 if (!chan) throw new Error("could not fetch comics channel");
                 if (chan.type != "GUILD_TEXT")
                     throw new Error("comics channel has unexpected type " + chan.type);
 
                 await (<TextChannel>chan).send({
-                    content: process.env.COMIC_EXTRA_CONTENT!,
+                    content: this.config.extraContent,
                     embeds: [embed],
                 });
                 comicState.maxPageId = latest.id;
@@ -65,10 +60,11 @@ export default class ComicPoller extends Module {
 interface PolledComic {
     source: ComicSource;
     url: string;
-    maxPageId: number;
 }
 export interface ComicConfig {
     comics: { [key: string]: PolledComic };
+    channelId: string;
+    extraContent: string;
 }
 export interface ComicStore {
     comics: { [key: string]: { maxPageId: number; } }
