542c7d95faf0c8ada82b6272fea4a2284344b1ee
ckie <git-525ff67@ckie.dev>
2021-12-06 20:12:55 +0200

    discord/comic: catch error in case one fetcher fails

--- src/discord/modules/comic.ts
+++ src/discord/modules/comic.ts
@@ -28,30 +28,34 @@ export default class ComicPoller extends Module {
 
     async pollComics() {
         for (let key in this.config.comics) {
-            const comic = this.config.comics[key];
-            if (!this.data.comics[key]) this.data.comics[key] = { maxPageId: 0 };
-            const comicState = this.data.comics[key];
-            logger.trace(`now polling ${key} with ${comic.source}`);
-            const meta = await fetchers[comic.source](new URL(comic.url))();
-            const latest = meta.pages[meta.pages.length - 1];
+            try {
+                const comic = this.config.comics[key];
+                if (!this.data.comics[key]) this.data.comics[key] = { maxPageId: 0 };
+                const comicState = this.data.comics[key];
+                logger.trace(`now polling ${key} with ${comic.source}`);
+                const meta = await fetchers[comic.source](new URL(comic.url))();
+                const latest = meta.pages[meta.pages.length - 1];
 
-            if (comicState.maxPageId < latest.id) {
-                const embed = new MessageEmbed();
-                embed.setAuthor(meta.title, undefined, latest.url);
-                embed.setImage(meta.bannerURL);
-                embed.setURL(latest.url);
-                embed.setTitle(latest.title);
+                if (comicState.maxPageId < latest.id) {
+                    const embed = new MessageEmbed();
+                    embed.setAuthor(meta.title, undefined, latest.url);
+                    embed.setImage(meta.bannerURL);
+                    embed.setURL(latest.url);
+                    embed.setTitle(latest.title);
 
-                const chan = await this.client.channels.fetch(this.config.channelId);
-                if (!chan) throw new Error("could not fetch comics channel");
-                if (chan.type != "GUILD_TEXT")
-                    throw new Error("comics channel has unexpected type " + chan.type);
+                    const chan = await this.client.channels.fetch(this.config.channelId);
+                    if (!chan) throw new Error("could not fetch comics channel");
+                    if (chan.type != "GUILD_TEXT")
+                        throw new Error("comics channel has unexpected type " + chan.type);
 
-                await (<TextChannel>chan).send({
-                    content: this.config.extraContent,
-                    embeds: [embed],
-                });
-                comicState.maxPageId = latest.id;
+                    await (<TextChannel>chan).send({
+                        content: this.config.extraContent,
+                        embeds: [embed],
+                    });
+                    comicState.maxPageId = latest.id;
+                }
+            } catch (err: unknown) {
+                logger.error(`Error while fetching comic ${key}:`, err);
             }
         }
     }
