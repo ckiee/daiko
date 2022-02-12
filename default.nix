d67e0bad7ae651767183b4dd98350f3fea920fd5
ckie <git-525ff67@ckie.dev>
2022-02-12 13:02:32 +0200

    nix: bcrypt hell

--- default.nix
+++ default.nix
@@ -13,4 +13,25 @@ in yarn2nix.mkYarnPackage rec {
   yarnLock = ./yarn.lock;
   yarnNix = ./yarn.nix;
   buildPhase = "yarn --offline run postinstall";
+  pkgConfig.bcrypt = let
+    arch = if stdenv.hostPlatform.system == "x86_64-linux" then
+      "linux-x64"
+    else
+      throw "Unsupported architecture: ${stdenv.hostPlatform.system}";
+    bcrypt_version = "5.0.1";
+    bcrypt_lib = fetchurl {
+      url =
+        "https://github.com/kelektiv/node.bcrypt.js/releases/download/v${bcrypt_version}/bcrypt_lib-v${bcrypt_version}-napi-v3-${arch}-glibc.tar.gz";
+      sha256 = "3R3dBZyPansTuM77Nmm3f7BbTDkDdiT2HQIrti2Ottc=";
+    };
+  in {
+    buildInputs = [ jq gnutar ];
+    postInstall = ''
+      if [ "${bcrypt_version}" != "$(cat package.json | jq -r .version)" ]; then
+        echo "Mismatching version, please update bcrypt in derivation"
+        exit
+      fi
+      mkdir -p ./lib/binding && tar -C ./lib/binding -xf ${bcrypt_lib}
+    '';
+  };
 }
