1092c8bb88a61f4298d613b0341dfdd408b04840
ckie <git-525ff67@ckie.dev>
2022-05-31 22:37:30 +0300

    discord/lines: better lines

--- src/discord/modules/lines.ts
+++ src/discord/modules/lines.ts
@@ -4,6 +4,7 @@ import {
     Module,
     command,
     CommonInhibitors,
+    optional,
 } from "cookiecord";
 import { config } from "../../config";
 
@@ -15,24 +16,40 @@ export default class LinesModule extends Module {
 
     START = 1615613520000;
     SCH_END = 1718166720000;
+    MAIN_END = 1697414400000;
     DAY = 86400000;
 
     @command({ description: "my lil lines..", inhibitors: [CommonInhibitors.botAdminsOnly] })
-    async lines(msg: Message) {
-        await msg.reply(this.getLineMessage());
+    async lines(msg: Message, @optional filter?: string) {
+        await msg.reply(this.getLineMessage(filter ? [...filter] : [..."*Ms"]));
     }
 
-    public getLineMessage(): string {
-        let col = 0, buf = "", t = 1196, d = Math.round((Date.now() - this.START) / this.DAY);
-        while (t > 0) {
-            if (d > 0) d--;
-            t--;
-            col++;
-            buf += d > 0 ? "*" : "_"
-            if (col % 80 == 0) buf += "\n"
-        }
+    public getLineMessage(showLabels: string[]): string {
+        const msToDays = (ms: number) => Math.round(ms / this.DAY);
+        const events = ([
+            ["*", Date.now() - this.START],
+            ["M", this.MAIN_END - Date.now()],
+            ["s", this.SCH_END - Date.now()]
+        ] as const)
+            .filter(([l, _]) => showLabels.includes(l))
+            .map(([_, ms]) => [_, msToDays(ms)]) as [string, number][];
+
+        const displayDayCount = Math.max(...events.map(x => x[1]));
+
+        const buf = Array(displayDayCount).fill("?").map((_, i) => {
+            const progressEvent = events[0][1] >= i;
+            const maybeEvent = events.slice(1).filter(([label, ei]) => ei == i + 1)[0];
+            return (maybeEvent ? maybeEvent[0] : (progressEvent ? events[0][0] : "_")) +
+                ((i + 1) % 80 == 0 ? "\n" : "");
+        }).join("");
+
         const BQ = "```";
-        return `${BQ}\n${buf}\n${BQ}`;
+        return `${BQ}
+${buf}
+
+${events.map(([l, d]) => `${l}: ${d}`).join("\t\t")}
+${events.map(([l, d]) => `${l}-${events[0][0]}: ${d - events[0][1]}`).join("\t")}
+${BQ}`;
     }
 }
 
